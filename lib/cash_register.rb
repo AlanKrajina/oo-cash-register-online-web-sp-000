fa17b7a81cf7b29698b27b05468bde1683bd0d48
Alan Krajina <alankrajina88@gmail.com>
2019-05-23 16:46:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,11 +23,10 @@ class CashRegister
     if @discount == 0
       "There is no discount to apply."
     else
-    trans = self.total * @discount
-    trans2 = trans / 100
-    self.total -= trans2
-    "After the discount, the total comes to $#{self.total}."
-
+      trans = self.total * @discount
+      trans2 = trans / 100
+      self.total -= trans2
+      "After the discount, the total comes to $#{self.total}."
     end
   end
   
